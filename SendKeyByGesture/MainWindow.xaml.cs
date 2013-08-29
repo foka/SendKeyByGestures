05a1c0b9f963ff29bf3e9d3c2336ad24120df7eb
foka <michal.foka.wlodarczyk@gmail.com>
2013-08-29 08:36:39 +0200

    SendKeys POC

---
+++ SendKeyByGesture/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+namespace SendKeyByGesture
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var timer = new Timer(1000);
+			timer.Elapsed += (_, __) => SendKeys.SendWait("{F5}");
+			timer.Start();
+		}
+	}
+}
