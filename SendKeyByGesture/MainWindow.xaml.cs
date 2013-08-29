2bb6fa48d0c90290077f6d6e68da4481287dad82
foka <michal.foka.wlodarczyk@gmail.com>
2013-08-29 11:48:06 +0200

    Kinect set up

--- SendKeyByGesture/MainWindow.xaml.cs
+++ SendKeyByGesture/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Forms;
-using Timer = System.Timers.Timer;
+﻿using System.Windows;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Samples.Kinect.WpfViewers;
 
 namespace SendKeyByGesture
 {
@@ -10,11 +12,63 @@ namespace SendKeyByGesture
 	{
 		public MainWindow()
 		{
-			InitializeComponent();
+			KinectSensorChooser = new KinectSensorChooser();
+			KinectSensorManager = new KinectSensorManager();
 
-			var timer = new Timer(1000);
-			timer.Elapsed += (_, __) => SendKeys.SendWait("{F5}");
-			timer.Start();
+			InitializeComponent();
+		}
+
+
+		public KinectSensorChooser KinectSensorChooser { get; private set; }
+		public KinectSensorManager KinectSensorManager { get; private set; }
+
+
+		private void StartKinect(KinectSensor sensor)
+		{
+			sensor.Start();
+
+			KinectSensorManager.ElevationAngle = sensor.ElevationAngle;
+			KinectSensorManager.KinectSensor = sensor;
+			KinectSensorManager.SkeletonStreamEnabled = true;
+//			KinectSensorManager.TransformSmoothParameters = new TransformSmoothParameters
+//			{
+//				Smoothing = 0.99f,
+//				Correction = 0.1f,
+//				Prediction = 0.1f,
+//				JitterRadius = 0.05f,
+//				MaxDeviationRadius = 0.05f,
+//			};
+			KinectSensorManager.SkeletonTrackingMode = SkeletonTrackingMode.Seated;
+			KinectSensorManager.SkeletonEnableTrackingInNearMode = true;
+//			KinectSensorManager.DepthStreamEnabled = true;
+			KinectSensorManager.ColorStreamEnabled = true;
+		}
+
+		private void StopKinect(KinectSensor sensor)
+		{
+			sensor.Stop();
+		}
+
+
+		private void KinectChanged(object sender, KinectChangedEventArgs e)
+		{
+			if (e.OldSensor != null)
+				StopKinect(e.OldSensor);
+			if (e.NewSensor != null)
+				StartKinect(e.NewSensor);
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			kinectSensorChooserUI.KinectSensorChooser = KinectSensorChooser;
+			KinectSensorChooser.KinectChanged += KinectChanged;
+			KinectSensorChooser.Start();
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			if (KinectSensorChooser.Kinect != null)
+				StopKinect(KinectSensorChooser.Kinect);
 		}
 	}
 }
