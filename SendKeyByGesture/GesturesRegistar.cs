fcb33d03560157f60a0a1e3daf0c3b238428ab20
foka <michal.foka.wlodarczyk@gmail.com>
2013-08-29 14:46:04 +0200

    Standard Fizbin gestures

---
+++ SendKeyByGesture/GesturesRegistar.cs
@@ -0,0 +1,87 @@
+﻿using Fizbin.Kinect.Gestures;
+using Fizbin.Kinect.Gestures.Segments;
+
+namespace SendKeyByGesture
+{
+	public class GesturesRegistar
+	{
+		public static void RegisterGestures(GestureController gestureController)
+		{
+			var joinedhandsSegments = new IRelativeGestureSegment[20];
+			var joinedhandsSegment = new JoinedHandsSegment1();
+			for (int i = 0; i < 20; i++)
+			{
+				// gesture consists of the same thing 10 times 
+				joinedhandsSegments[i] = joinedhandsSegment;
+			}
+			gestureController.AddGesture("JoinedHands", joinedhandsSegments);
+
+			var menuSegments = new IRelativeGestureSegment[20];
+			var menuSegment = new MenuSegment1();
+			for (int i = 0; i < 20; i++)
+			{
+				// gesture consists of the same thing 20 times 
+				menuSegments[i] = menuSegment;
+			}
+			gestureController.AddGesture("Menu", menuSegments);
+
+			var swipeleftSegments = new IRelativeGestureSegment[3];
+			swipeleftSegments[0] = new SwipeLeftSegment1();
+			swipeleftSegments[1] = new SwipeLeftSegment2();
+			swipeleftSegments[2] = new SwipeLeftSegment3();
+			gestureController.AddGesture("SwipeLeft", swipeleftSegments);
+
+			var swiperightSegments = new IRelativeGestureSegment[3];
+			swiperightSegments[0] = new SwipeRightSegment1();
+			swiperightSegments[1] = new SwipeRightSegment2();
+			swiperightSegments[2] = new SwipeRightSegment3();
+			gestureController.AddGesture("SwipeRight", swiperightSegments);
+
+			var waveRightSegments = new IRelativeGestureSegment[6];
+			var waveRightSegment1 = new WaveRightSegment1();
+			var waveRightSegment2 = new WaveRightSegment2();
+			waveRightSegments[0] = waveRightSegment1;
+			waveRightSegments[1] = waveRightSegment2;
+			waveRightSegments[2] = waveRightSegment1;
+			waveRightSegments[3] = waveRightSegment2;
+			waveRightSegments[4] = waveRightSegment1;
+			waveRightSegments[5] = waveRightSegment2;
+			gestureController.AddGesture("WaveRight", waveRightSegments);
+
+			var waveLeftSegments = new IRelativeGestureSegment[6];
+			var waveLeftSegment1 = new WaveLeftSegment1();
+			var waveLeftSegment2 = new WaveLeftSegment2();
+			waveLeftSegments[0] = waveLeftSegment1;
+			waveLeftSegments[1] = waveLeftSegment2;
+			waveLeftSegments[2] = waveLeftSegment1;
+			waveLeftSegments[3] = waveLeftSegment2;
+			waveLeftSegments[4] = waveLeftSegment1;
+			waveLeftSegments[5] = waveLeftSegment2;
+			gestureController.AddGesture("WaveLeft", waveLeftSegments);
+
+			var zoomInSegments = new IRelativeGestureSegment[3];
+			zoomInSegments[0] = new ZoomSegment1();
+			zoomInSegments[1] = new ZoomSegment2();
+			zoomInSegments[2] = new ZoomSegment3();
+			gestureController.AddGesture("ZoomIn", zoomInSegments);
+
+			var zoomOutSegments = new IRelativeGestureSegment[3];
+			zoomOutSegments[0] = new ZoomSegment3();
+			zoomOutSegments[1] = new ZoomSegment2();
+			zoomOutSegments[2] = new ZoomSegment1();
+			gestureController.AddGesture("ZoomOut", zoomOutSegments);
+
+			var swipeUpSegments = new IRelativeGestureSegment[3];
+			swipeUpSegments[0] = new SwipeUpSegment1();
+			swipeUpSegments[1] = new SwipeUpSegment2();
+			swipeUpSegments[2] = new SwipeUpSegment3();
+			gestureController.AddGesture("SwipeUp", swipeUpSegments);
+
+			var swipeDownSegments = new IRelativeGestureSegment[3];
+			swipeDownSegments[0] = new SwipeDownSegment1();
+			swipeDownSegments[1] = new SwipeDownSegment2();
+			swipeDownSegments[2] = new SwipeDownSegment3();
+			gestureController.AddGesture("SwipeDown", swipeDownSegments);
+		}
+	}
+}
--- SendKeyByGesture/MainWindow.xaml.cs
+++ SendKeyByGesture/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using Fizbin.Kinect.Gestures;
 using Microsoft.Kinect;
 using Microsoft.Kinect.Toolkit;
 using Microsoft.Samples.Kinect.WpfViewers;
@@ -21,27 +23,65 @@ namespace SendKeyByGesture
 
 		public KinectSensorChooser KinectSensorChooser { get; private set; }
 		public KinectSensorManager KinectSensorManager { get; private set; }
+		private Skeleton[] skeletons = new Skeleton[0];
+		private GestureController gestureController;
 
 
 		private void StartKinect(KinectSensor sensor)
 		{
 			sensor.Start();
+			sensor.SkeletonFrameReady += sensor_SkeletonFrameReady;
 
 			KinectSensorManager.ElevationAngle = sensor.ElevationAngle;
 			KinectSensorManager.KinectSensor = sensor;
 			KinectSensorManager.SkeletonStreamEnabled = true;
-//			KinectSensorManager.TransformSmoothParameters = new TransformSmoothParameters
-//			{
-//				Smoothing = 0.99f,
-//				Correction = 0.1f,
-//				Prediction = 0.1f,
-//				JitterRadius = 0.05f,
-//				MaxDeviationRadius = 0.05f,
-//			};
-			KinectSensorManager.SkeletonTrackingMode = SkeletonTrackingMode.Seated;
+			KinectSensorManager.TransformSmoothParameters = new TransformSmoothParameters
+			{
+				Smoothing = 0.99f,
+				Correction = 0.1f,
+				Prediction = 0.1f,
+				JitterRadius = 0.05f,
+				MaxDeviationRadius = 0.05f,
+			};
+//			KinectSensorManager.SkeletonTrackingMode = SkeletonTrackingMode.Seated;
 			KinectSensorManager.SkeletonEnableTrackingInNearMode = true;
-//			KinectSensorManager.DepthStreamEnabled = true;
+			KinectSensorManager.DepthStreamEnabled = true;
 			KinectSensorManager.ColorStreamEnabled = true;
+
+			gestureController = new GestureController();
+			gestureController.GestureRecognized += OnGestureRecognized;
+			GesturesRegistar.RegisterGestures(gestureController);
+		}
+
+		private void OnGestureRecognized(object sender, GestureEventArgs e)
+		{
+			Debug.WriteLine(e.GestureName + " " + e.TrackingId);
+		}
+
+		private void sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+		{
+			using (SkeletonFrame frame = e.OpenSkeletonFrame())
+			{
+				if (frame == null)
+					return;
+
+				// resize the skeletons array if needed
+				if (skeletons.Length != frame.SkeletonArrayLength)
+					skeletons = new Skeleton[frame.SkeletonArrayLength];
+
+				// get the skeleton data
+				frame.CopySkeletonDataTo(skeletons);
+
+				foreach (var skeleton in skeletons)
+				{
+					// skip the skeleton if it is not being tracked
+					if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+						continue;
+
+					// update the gesture controller
+					gestureController.UpdateAllGestures(skeleton);
+				}
+			}
 		}
 
 		private void StopKinect(KinectSensor sensor)
